99f234aa24c41b3a9c225f662481c376167b8a65
Dlurak <84224239+Dlurak@users.noreply.github.com>
2024-01-26 18:42:39 +0100

    Implement synchronisation between tabs and multiple stores using the same localstorage key

--- src/lib/stores/localstorage.ts
+++ src/lib/stores/localstorage.ts
@@ -1,32 +1,38 @@
-// import { parseString } from '$lib/json/stringParse.js';
+import { browser } from '$app/environment';
 import { readFromLocalStorage, writeToLocalStorage } from '$lib/localstorage/index.js';
 import { writable } from 'svelte/store';
+
+function addWindowEventListener(
+	key: string,
+	set: (this: void, value: unknown) => void,
+	fallback: unknown
+) {
+	if (!browser) return;
+	if (!window) return;
+
+	const update = () => set(readFromLocalStorage(key, fallback));
+
+	window.addEventListener('storage', ({ storageArea }) => {
+		if (!storageArea || storageArea !== localStorage) return;
+		update();
+	});
+
+	document.addEventListener('internalLocalStorageChange', update);
+}
 
 export function localstorage(key: string, fallback: unknown) {
 	const value = readFromLocalStorage(key, fallback);
 	const { subscribe, set } = writable(value);
 
-	// if (window) {
-	// 	window.addEventListener('storage', ({ storageArea, oldValue, newValue }) => {
-	// 		if (!storageArea || storageArea !== localStorage) return;
-	//
-	// 		const parsedOldValue = parseString(oldValue || '{}') as Record<string, unknown>;
-	// 		const parsedNewValue = parseString(newValue || '{}') as Record<string, unknown>;
-	//
-	// 		const oldValueKey = parsedOldValue[key];
-	// 		const newValueKey = parsedNewValue[key];
-	//
-	// 		if (oldValueKey === newValueKey) return;
-	//
-	// 		set(newValueKey);
-	// 	});
-	// }
+	addWindowEventListener(key, set, fallback);
 
 	return {
 		subscribe,
 		set: (newValue: unknown) => {
 			set(newValue);
+
 			writeToLocalStorage(key, newValue);
+			document.dispatchEvent(new Event('internalLocalStorageChange'));
 		}
 	};
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,21 +2,41 @@
 	import { localstorage } from '$lib/index.js';
 
 	let store = localstorage('demo', 1);
+
+	let store2 = localstorage('demo', 1);
+
+	store.subscribe(console.log);
 </script>
 
-<p>{$store}</p>
-<button
-	on:click={() => {
-		store.set($store + 1);
-	}}
->
-	Inc
-</button>
-
-<button
-	on:click={() => {
-		window.localStorage.setItem('demo', `${$store + 1}`);
-	}}
->
-	Increment directly (not through the store)
-</button>
+<table>
+	<thead>
+		<tr>
+			<th>Store 1</th>
+			<th>Store 2</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td>{$store}</td>
+			<td>{$store2}</td>
+		</tr>
+		<tr>
+			<td>
+				<button
+					on:click={() => {
+						store.set($store + 1);
+					}}
+				>
+					Increment store 1
+				</button>
+				<button
+					on:click={() => {
+						store.set($store2 + 1);
+					}}
+				>
+					Increment store 2
+				</button>
+			</td>
+		</tr>
+	</tbody>
+</table>
