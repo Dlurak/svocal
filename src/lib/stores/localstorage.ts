41cf6bee8f7f261517f228638195acfe9229805f
Dlurak <84224239+Dlurak@users.noreply.github.com>
2024-02-27 09:55:46 +0000

    Add JS-Doc comments

--- src/lib/stores/localstorage.ts
+++ src/lib/stores/localstorage.ts
@@ -29,12 +29,23 @@ export function localstorage<T>(key: string, fallback: T) {
 
 	return {
 		subscribe,
+		/**
+		 * Set a new value into the store,
+		 * this will also affect all other svocal stores using the same key
+		 * and will write to the localstorage
+		 * @param newValue The new value to set
+		 */
 		set: (newValue: T) => {
 			set(newValue);
 
 			writeToLocalStorage(key, newValue);
 			document.dispatchEvent(new Event('internalLocalStorageChange'));
 		},
+		/**
+		 * Update the store using a function
+		 * It will affect other stores with the same key and write to the localstorage
+		 * @param newValueFn A function which return value will be the new value
+		 */
 		update: (newValueFn: (oldVal: T) => T) => {
 			const newValue = newValueFn(get(writableStore));
 			set(newValue);
